18f23956b8e05da0dcdfac7e90ee8196faf86b99
PC-26 <PC-26@LAPTOP-HCPVHVO2>
2024-10-20 17:47:21 +0800

    Add project files.

---
+++ CalcLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public double Div(int a, int b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return (double)a / b;
+        }
+        public bool IsEven(int number) => number % 2 == 0;
+    }
---
+++ CalcTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace CalcAPI_NunitTesting
+{
+    [TestFixture]
+    public class CalcTest
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void Add_WhenCalled_ReturnsCorrectSum()
+        {
+            Assert.AreEqual(100, _calculator.Add(60, 40));
+        }
+
+        [Test]
+        public void Sub_WhenCalled_ReturnsCorrectDifference()
+        {
+            Assert.AreEqual(50, _calculator.Sub(90, 40));
+        }
+
+        [Test]
+        public void Mul_WhenCalled_ReturnsCorrectProduct()
+        {
+            Assert.AreEqual(180, _calculator.Mul(60, 3));
+        }
+
+        [Test]
+        public void Div_WhenCalledWithNonZero_ReturnsCorrectQuotient()
+        {
+            Assert.AreEqual(3, _calculator.Div(9, 3));
+        }
+
+        [Test]
+        public void IsEven_WhenCalledWithEvenNumber_ReturnsTrue()
+        {
+            Assert.IsTrue(_calculator.IsEven(4));
+        }
+
+        [Test]
+        public void IsEven_WhenCalledWithOddNumber_ReturnsFalse()
+        {
+            Assert.IsFalse(_calculator.IsEven(5));
+        }
+    }
+}
